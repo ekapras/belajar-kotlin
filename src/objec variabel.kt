a882ef4258e6f685506095f169b8000d3689bc7b
unknown <ekapras19@gmail.com>
2017-10-27 14:07:24 +0700

    update object variabel

--- src/objec variabel.kt
+++ src/objec variabel.kt
@@ -2,6 +2,21 @@ class Mobil2{
     var mesin=1
     var roda=4
     var body=1
+}
 
-    roda avasnsa=4
+fun main(args: Array<String>) {
+    var avanza = Mobil2()
+    var innova = Mobil2()
+
+    println("Roda avanza : " + avanza.roda)
+    println("Roda innova : " + innova.roda)
+    avanza.roda = 5
+    println("Roda avanza : " + avanza.roda)
+    println("Roda innova : " + innova.roda)
+
+    println("Mesin avanza : " + avanza.mesin)
+    println("Mesin innova : " + innova.mesin)
+    avanza.mesin = 2
+    println("Mesin avanza : " + avanza.mesin)
+    println("Mesin innova : " + innova.mesin)
 }
