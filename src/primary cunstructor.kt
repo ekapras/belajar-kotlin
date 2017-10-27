d865473b0ee8cac8c8aa5ffad73c65e36c2ad22f
unknown <ekapras19@gmail.com>
2017-10-27 15:26:27 +0700

    update primary constructor dan inheritance

---
+++ src/primary cunstructor.kt
@@ -0,0 +1,15 @@
+class Mobil4(mesin: Int, roda: Int, body: Int) {
+
+    init {
+        println("Mesin: $mesin, Roda: $roda, Body: $body")
+    }
+
+    val mesinMobil = mesin
+    val rodaMobil = roda
+    val bodyMobil = body
+
+}
+
+fun main(args: Array<String>) {
+
+}
